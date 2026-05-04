de8413b23a5a5224d87700f8c378ffe76a2e49d7
Naza Velloso <nazarenavelloso@gmail.com>
2026-05-04 20:55:58 -0300

    cambios ej 04

--- Dsw2026Ej8/Program.cs
+++ Dsw2026Ej8/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Dsw2026Ej8.ej01;
 using Dsw2026Ej8.ej02;
 using Dsw2026Ej8.ej03;
+using Dsw2026Ej8.ej04;
 
 namespace Dsw2026Ej8
 {
@@ -8,25 +9,32 @@ namespace Dsw2026Ej8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("PRUEBAS EJ 01");
+            Console.WriteLine("----- PRUEBAS EJ 01 -----\n");
             Product a = new Product(123l, "sopa", 44M);
             Product b = new Product(988l, "fideo", 90.50M);
             Console.WriteLine(ProductHelper.ObtenerEtiquetaProducto(a));
             Console.WriteLine(ProductHelper.ObtenerEtiquetaProducto(b));
-            Console.WriteLine("--------");
 
-            Console.WriteLine("PRUEBAS EJ 02");
+            Console.WriteLine("\n----- PRUEBAS EJ 02 -----\n");
             Product2 c = new Product2(1001, "fideos", 5, 20.50M);
             Console.WriteLine(Problema2.CrearResumenVenta(c));
             Product2 d = new Product2(3021, "salsa de tomate", 0, 15M);
             Console.WriteLine(Problema2.CrearResumenVenta(d));
-            Console.WriteLine("---------");
 
-
-            Console.WriteLine("PRUEBAS EJ 03");
+            Console.WriteLine("\n----- PRUEBAS EJ 03 -----\n");
             Product e = new Product(2002, "sopa paraguaya", 20.90M);
             Console.WriteLine(Problema3.CompararCopias(34,e));
-            Console.WriteLine("---------");
+
+            Console.WriteLine("\n----- PRUEBAS EJ 04 -----\n");
+            Console.WriteLine(problema4.CalcularPromedio(10, 8, 9));
+            Console.WriteLine(problema4.CalcularPromedio(null, null,null));
+            Console.WriteLine(problema4.CalcularPromedio(12, 8, 9));
+            Console.WriteLine(problema4.CalcularPromedio(10, null, 9));
+            Console.WriteLine(problema4.CalcularPromedio(null, 0, 5));
+
+          
+
+
         }
     }
 
---
+++ Dsw2026Ej8/ej04/problema4.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8.ej04
+{
+    internal class problema4
+    {
+        public static double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+            int contador = 0;
+            int suma =0;
+            double promedio = 0;
+            
+            if(nota1 is >= 0 and <=10)
+            {
+                suma+= nota1.Value;
+                ++contador;
+
+            } 
+
+            if (nota2 is >= 0 and <= 10)
+            {
+                suma+=nota2.Value;
+                ++contador;
+
+            }
+           
+            if (nota3 is >= 0 and <= 10)
+            {
+                suma+=nota3.Value;
+                ++contador;
+
+            } 
+
+            if(contador == 0) {
+
+                Console.WriteLine($"Hay {contador} notas válidas");
+                Console.WriteLine($"El promedio es: {promedio}");
+                return promedio;
+                
+            }
+
+            promedio = suma / contador;
+
+            Console.WriteLine($"Hay {contador} notas válidas");
+            Console.WriteLine($"El promedio es: {promedio}");
+            return promedio;
+            
+
+        }
+    }
+}
